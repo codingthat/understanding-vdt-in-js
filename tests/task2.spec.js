4c7228f692d92109dc972c9eceaec14ce31f3b9d
Kevin Bloch <kev@codingthat.com>
2024-04-01 14:58:57 +0200

    Refactored common test code

--- tests/task2.spec.js
+++ tests/task2.spec.js
@@ -1,23 +1,13 @@
 'use strict'
-
-var o = require('ospec'),
-    fs = require('fs'),
-    path = require('path'),
-    taskName = path.basename(__filename, '.spec.js')
+var o = require('ospec')
 
 o.spec('first array declarations', function() {
-    let code,
-        start = fs.readFileSync(`${__dirname}/../.start/${taskName}.js`).toString().trim()
-    o.before(function getDirAndLoadCode() {
-        code = fs.readFileSync(`${__dirname}/../${process.env.TEST_TARGET_DIR ?? '.solution'}/${taskName}.js`).toString()
-    })
+    let taskName = require('path').basename(__filename, '.spec.js'),
+        code = require('../test-common')(taskName)
     o('names array is declared (exactly once)', function() {
         o(code.match(/let\s+ingredientNames\s*=\s*\["water",\s*"lemon juice concentrate"\s*\]\s*;\s*$/m)?.length).equals(1)
     })
     o('quantity variable is declared (exactly once)', function() {
         o(code.match(/let\s+ingredientQuantities\s*=\s*\["1 cup",\s*"1 tbsp"\s*\]\s*;\s*$/m)?.length).equals(1)
-    })
-    o('code is written above comment line as required (original code is left untouched)', function() {
-        o(code.includes(start)).equals(true)
     })
 })
