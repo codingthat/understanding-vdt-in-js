a9bad0b56379be99a140caa686a81d6cfe3be187
Kevin Bloch <kev@codingthat.com>
2024-04-01 15:00:59 +0200

    Hide refactored test file

--- tests/task1.spec.js
+++ tests/task1.spec.js
@@ -3,7 +3,7 @@ var o = require('ospec')
 
 o.spec('first variable declarations', function() {
     let taskName = require('path').basename(__filename, '.spec.js'),
-        code = require('../test-common')(taskName)
+        code = require('../.test-common')(taskName)
     o('name variable is declared (exactly once)', function() {
         o(code.match(/\s*let\s+ingredientName\s*=\s*"water"\s*;\s*$/m)?.length).equals(1)
     })
--- tests/task2.spec.js
+++ tests/task2.spec.js
@@ -3,7 +3,7 @@ var o = require('ospec')
 
 o.spec('first array declarations', function() {
     let taskName = require('path').basename(__filename, '.spec.js'),
-        code = require('../test-common')(taskName)
+        code = require('../.test-common')(taskName)
     o('names array is declared (exactly once)', function() {
         o(code.match(/let\s+ingredientNames\s*=\s*\["water",\s*"lemon juice concentrate"\s*\]\s*;\s*$/m)?.length).equals(1)
     })
