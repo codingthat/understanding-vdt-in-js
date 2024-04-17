5ed07d97826f198846dbcaea2f3b062e974cd944
Kevin Bloch <kev@codingthat.com>
2024-04-17 11:01:57 +0200

    Make testrunner match new build process and make task2-3 specs match adjusted tasks

--- tests/task2.spec.js
+++ tests/task2.spec.js
@@ -8,6 +8,6 @@ o.spec('first array declarations', function() {
         o(code.match(/let\s+ingredientNames\s*=\s*\["water",\s*"lemon juice concentrate"\s*\]\s*;\s*$/m)?.length).equals(1)
     })
     o('quantities array is declared (exactly once)', function() {
-        o(code.match(/let\s+ingredientQuantities\s*=\s*\["1 cup",\s*"1 tbsp"\s*\]\s*;\s*$/m)?.length).equals(1)
+        o(code.match(/let\s+ingredientQuantities\s*=\s*\["1 cup",\s*"1\.5 tbsp"\s*\]\s*;\s*$/m)?.length).equals(1)
     })
 })
--- tests/task3.spec.js
+++ tests/task3.spec.js
@@ -8,6 +8,6 @@ o.spec('first set declarations with a duplicate ingredient', function() {
         o(code.match(/let\s+ingredientNames\s*=\s*new\s+Set\(\s*\["water",\s*"lemon juice concentrate"\s*,\s*"lemon juice concentrate"\s*\]\s*\)\s*;\s*$/m)?.length).equals(1)
     })
     o('quantities set is declared (exactly once)', function() {
-        o(code.match(/let\s+ingredientQuantities\s*=\s*new\s+Set\(\s*\["1 cup",\s*"1 tbsp"\s*,\s*"1 tbsp"\s*\]\s*\)\s*;\s*$/m)?.length).equals(1)
+        o(code.match(/let\s+ingredientQuantities\s*=\s*new\s+Set\(\s*\["1 cup",\s*"1\.5 tbsp"\s*,\s*"1\.5 tbsp"\s*\]\s*\)\s*;\s*$/m)?.length).equals(1)
     })
 })
